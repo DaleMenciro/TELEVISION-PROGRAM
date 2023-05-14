ab481aff5d6c851d19be6255f0687f005bcc3ed4
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 03:46:00 +0800

    setting parameters on setting and getting channel

--- Television.py
+++ Television.py
@@ -12,13 +12,16 @@ class TV:
 
     def turn_on(self) -> None:
         self.on = True #True to turn on TV
-    
+        
     def turn_off(self) -> None:
         self.on = False #False to turn off TV
 
     def get_channel(self) -> int:
+        return self.channel
 
     def set_channel(self,channel_number) -> int:
+        if channel_number >= 1 and channel_number <= 120: #Set channel to new value within the range (1-120)
+            self.channel = channel_number
     
     def get_volume(self) -> int:
 
