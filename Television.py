52baf570189c523ac4a97030132ec0c0461511f7
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 03:36:50 +0800

    defining functions according to UML Class Diagram

--- Television.py
+++ Television.py
@@ -9,3 +9,23 @@ class TV:
         self.channel = 1 #Channel 1 to be default
         self.volumeLevel = 1 #Volume 1 to be the dafault
         self.on = False # TV is off at default
+
+    def turn_on(self) -> None:
+    
+    def turn_off(self) -> None:
+
+    def get_channel(self) -> int:
+
+    def set_channel(self,channel_number) -> int:
+    
+    def get_volume(self) -> int:
+
+    def set_volume(self,volume_level) -> int:
+
+    def channel_uP(self) -> None:
+
+    def channel_down(self) -> None:
+    
+    def volume_up(self) -> None:
+
+    def volume_down(self) -> None:
