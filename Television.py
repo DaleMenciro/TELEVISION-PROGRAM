4da87fc4ee452438ed9fc9ff3a42d1b56a24963b
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 02:57:07 +0800

    creative class TV

--- Television.py
+++ Television.py
@@ -4,4 +4,4 @@ channel - must be an int and current channel should range from 1-120
 volumelevel- must be an int and current volume level should range be from 1-7
 on - must be a bool, either on/off.
 '''
-
+class TV:
