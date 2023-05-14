397e0eebb68a1baf522e69b043808c7468bb523c
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 03:03:45 +0800

    Initializing TV objects and their default values

--- Television.py
+++ Television.py
@@ -1,7 +1,11 @@
 '''
 Acquire these arguments
 channel - must be an int and current channel should range from 1-120
-volumelevel- must be an int and current volume level should range be from 1-7
+volumeLevel- must be an int and current volume level should range be from 1-7
 on - must be a bool, either on/off.
 '''
 class TV:
+    def __init__(self):
+        self.channel = 1 #Channel 1 to be default
+        self.volumeLevel = 1 #Volume 1 to be the dafault
+        self.on = False # TV is off at default
