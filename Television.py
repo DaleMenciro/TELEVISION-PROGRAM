336de32c8813056d7b929ba328f83d91a9bee73a
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 02:55:24 +0800

    Setting parameters

---
+++ Television.py
@@ -0,0 +1,7 @@
+'''
+Acquire these arguments
+channel - must be an int and current channel should range from 1-120
+volumelevel- must be an int and current volume level should range be from 1-7
+on - must be a bool, either on/off.
+'''
+
