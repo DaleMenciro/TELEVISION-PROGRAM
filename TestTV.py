0b0f79910ba26428ccc197efbd10ad7da996c430
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 21:30:00 +0800

    initializing GUI

--- TestTV.py
+++ TestTV.py
@@ -2,3 +2,7 @@ import tkinter as tk
 from Television import TV
 
 class TV_GUI:
+    def __init__(self,master):
+        self.master= master
+        master.title("Test TV")
+        master.geometry("300x200")
