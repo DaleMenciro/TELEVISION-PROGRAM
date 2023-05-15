4349487b25602f3f4254389e7a1ba88fb6acdd2b
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 21:44:49 +0800

    Create TV 2 label to display channel and volume level

--- TestTV.py
+++ TestTV.py
@@ -27,6 +27,9 @@ class TV_GUI:
         self.TV_1_label = tk.Label(master, text=f"TV1's channel is {self.TV_1.get_channel()} and volume is {self.TV_1.get_volume()}")
         self.TV_1_label.pack
 
+        self.TV_2_label = tk.Label(master, text=f"TV2's channel is {self.TV_2.get_channel()} and volume is {self.TV_2.get_volume()}")
+        self.TV_2_label.pack
+
 root = tk.Tk()
 TestTV_GUI = TV_GUI(root)
 root.mainloop()
