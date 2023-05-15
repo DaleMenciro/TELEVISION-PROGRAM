f04acd867c5d165932463326afdfc52cd853cdc0
Dale Menciro <dale_menciro@protonmail.com>
2023-05-15 21:34:24 +0800

    adding color scheme

--- TestTV.py
+++ TestTV.py
@@ -7,6 +7,10 @@ class TV_GUI:
         master.title("Test TV")
         master.geometry("300x200")
 
+        self.primary_color = "#264653"
+        self.secondary_color = "#2a9d8f"
+        self.highlight_color = "#e9c46a"
+
 root = tk.Tk()
 TestTV_GUI = TV_GUI(root)
 root.mainloop()
